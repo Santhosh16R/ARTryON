144756eeff5f094951ae10f08a8894247757c193
Santhosh16R <103551648+Santhosh16R@users.noreply.github.com>
2024-04-01 18:14:05 +0530

    Update modelVisibility.js

--- CustomJs/modelVisibility.js
+++ CustomJs/modelVisibility.js
@@ -1,32 +1,72 @@
 // modelVisibility.js
 
+// export function setupModelVisibility() {
+//   document.addEventListener("DOMContentLoaded", function () {
+//     const list = ["glasses1", "glasses2","glasses3","glasses4","glasses5","glasses6", "hat1", "hat2", "earring"];
+//     const visibles = [false, false, false, false, false];
+//     const setVisible = (button, entities, visible) => {
+//       if (visible) {
+//         button.classList.add("selected");
+//       } else {
+//         button.classList.remove("selected");
+//       }
+//       entities.forEach((entity) => {
+//         entity.setAttribute("visible", visible);
+//       });
+//     }
+
+//     list.forEach((item, index) => {
+//       const button = document.querySelector("#" + item);
+//       const entities = document.querySelectorAll("." + item + "-entity");
+//       setVisible(button, entities, visibles[index]);
+//       button.addEventListener('click', () => {
+//         visibles[index] = !visibles[index];
+//         setVisible(button, entities, visibles[index]);
+//       });
+//     });
+//   });
+
+// }
+
 export function setupModelVisibility() {
   document.addEventListener("DOMContentLoaded", function () {
-    const list = ["glasses1", "glasses2","glasses3","glasses4","glasses5","glasses6", "hat1", "hat2", "earring"];
-    const visibles = [false, false, false, false, false];
+    const list = ["glasses1", "glasses2", "glasses3", "glasses4", "glasses5", "glasses6", "hat1", "hat2", "earring"];
+    let selectedIndex = -1;
+
     const setVisible = (button, entities, visible) => {
-      if (visible) {
-        button.classList.add("selected");
-      } else {
-        button.classList.remove("selected");
-      }
-      entities.forEach((entity) => {
-        entity.setAttribute("visible", visible);
+      button.classList.toggle("selected", visible);
+      entities.forEach(entity => {
+        entity.style.display = visible ? "block" : "none";
       });
-    }
+
+      entities.forEach((entity) => {
+                 entity.setAttribute("visible", visible);
+               });
+
+    };
 
     list.forEach((item, index) => {
       const button = document.querySelector("#" + item);
       const entities = document.querySelectorAll("." + item + "-entity");
-      setVisible(button, entities, visibles[index]);
       button.addEventListener('click', () => {
-        visibles[index] = !visibles[index];
-        setVisible(button, entities, visibles[index]);
+        if (selectedIndex !== index) {
+          if (selectedIndex !== -1) {
+            const prevButton = document.querySelector("#" + list[selectedIndex]);
+            const prevEntities = document.querySelectorAll("." + list[selectedIndex] + "-entity");
+            setVisible(prevButton, prevEntities, false);
+          }
+          selectedIndex = index;
+          setVisible(button, entities, true);
+        } else {
+          selectedIndex = -1;
+          setVisible(button, entities, false);
+        }
       });
     });
   });
-
 }
+
+
 setupModelVisibility();
 
 let buttonIdi, entityClassi;
